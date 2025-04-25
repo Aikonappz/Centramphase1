3306ddb5f5e2ffacd7894a6e4f94c04ddb85abec
Karthikeyan Subburaj <karthisubburaj2@gmail.com>
2025-04-25 23:03:03 +0530

    login api integration and session maintenance fix

--- src/core/data/api/index.ts
+++ src/core/data/api/index.ts
@@ -14,7 +14,7 @@ api.interceptors.request.use(
   (config: any): any => {
     const token = localStorage.getItem('token');
     if (token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `${token}`;
     }
     return config;
   },
